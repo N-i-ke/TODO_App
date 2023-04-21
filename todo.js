456ba7b931eb364df047da81407fa9528e2650ff
N-i-ke <82163707+N-i-ke@users.noreply.github.com>
2023-04-21 14:47:43 +0900

    Update_todo.js

--- todo.js
+++ todo.js
@@ -1,55 +1,43 @@
-//func todoAction() 
-function todoAction() { 
-    const buttonList = document.querySelectorAll('button');
+const addTodoButton = document.getElementById("new-todo");
+console.log(addTodoButton);
 
-    buttonList.forEach((button) => { 
-        button.addEventListener('click', () => { 
-            button.closest('.todo_done');
-            console.log(button.closest('.todo_done'));
-            // li.classList.add('done');
-        });
-    });
+//youtuber mu-zaru-channelを一部参考にソースコードは以下
+// https://github.com/moozaru-ch/mu-zaru-channel/blob/master/todo-javascript/day2/index.html
+
+// addTodoButton.addEventListner("click", () => {
+//   const input = document.querySelector("#new-todo");
+//   const text = input.value;
+
+//   //input参照してString型で返す
+//   input.value = "";
+
+//   const newLi = document.createElement("li");
+//   //inputで入ったものを入れる
+//   newLi.innerText = text;
+//   console.log(newLi);
+// });
+
+//newTODOの挙動
+function newTodoBtn(){ 
+  const input = document.getElementById('new-todo');
+  const text = input.value;
+  //input.valueに文字列を入れちゃう
+  input.value = '';
+  const newLi = document.createElement('li');
+  //liタグの中にtextを入れる
+  newLi.innerText = text;
+  console.log(input);
+  //ここでliタグを作ってるから
+  document.querySelector('ul').appendChild(newLi);
+  // newLi.appendChild(input);
 }
 
-// func increTODO() ボタンを押した時に新しいリストを作る関数
-function increTODO() {
-  //liタグを生成
-  const getTodo = document.getElementById("list_ID");
-  let getLi = document.createElement("li");
+function addNewBtn() { 
 
-  const getSen = document.getElementsByClassName("todo_sen")[0];
-  getSen.textContent = "todo_sentence";
-  console.log(getSen.textContent = "todo_sentence");
+  let newBtn = document.createElement('button');
+  newBtn.innerText = 'done';
 
-  const getNextSen = document.getElementsByClassName("next_todo_sen")[0];
-  getNextSen.textContent = "next_todo_sentence";
+  console.log(newBtn);
 
-  insertSentence();
-  //creatButton関数とinsert関数
-  creatButton();
-  //class next_todo_senを参照
-  nextInsertSen();
-
-  function creatButton() {
-    const getButton = document.createElement("button");
-    getLi.appendChild(getButton);
-  }
-
-  function insertSentence() {
-    const getSen = document.getElementsByClassName("todo_sen")[0];
-    getSen.textContent = "Todo {ここに四番目の数値を入れる} ";
-  }
-
-  function nextInsertSen() {
-    const nextSen = document.getElementsByClassName("next_todo_sen")[0];
-    nextSen.textContent = "とりあえずなんか入れる";
-  }
-
-  //一番最初はliタグがid = "list_ID"を参照して生成される
-  getTodo.appendChild(getLi);
-  getLi.appendChild(getSen)
-  getLi.appendChild(getNextSen);
-
-  //2回目以降のクリックで対応してないから
+  document.querySelector('ul').appendChild(newBtn)
 }
-
