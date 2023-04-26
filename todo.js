5fb14f6f6bcc2ff4b56677799fade54ea609cab0
N-i-ke <82163707+N-i-ke@users.noreply.github.com>
2023-04-26 18:36:28 +0900

    Update_todo.js
    追加されたボタンとリストに対して993pxをブレークポイントにレスポンシブ対応（予定）

--- todo.js
+++ todo.js
@@ -5,29 +5,41 @@ function clickBtn() {
   const Input = document.getElementById("new-todo");
   const Text = Input.value;
   Input.value = "";
-  
+
   const createBtn = document.createElement("button");
-  //material.js
-  createBtn.className = 'add_button col s2'
-  createBtn.innerText = "done";
+  // material.js framework col s2
+  createBtn.className = "add_button col s2";
+  createBtn.innerText = "完了";
 
   const createLi = document.createElement("li");
-  //material.js
-  createLi.className = 'add_li col s2'
+  // material.js framework col s2
+  createLi.className = "input-field col s2";
   createLi.innerText = Text;
 
   //clickされた時にline-throughを持ってなかったら付け加えて持ってたらつける
-  createBtn.onclick = function () { 
-    if (createLi.style.textDecoration === 'line-through') {
-      createLi.style.textDecoration = 'none';
-    } else { 
+  createBtn.onclick = function () {
+    if (createLi.style.textDecoration === "line-through") {
+      createLi.style.textDecoration = "none";
+    } else {
       createLi.style.textDecoration = "line-through";
     }
-  }
+  };
 
   document.getElementById("ul").appendChild(createLi);
   document.getElementById("ul").appendChild(createBtn);
-
-  console.log('TODOListは追加されています。');
 }
 
+
+
+  //追加されるTODOListのresponsive対応
+  window.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("resize", function () {
+  
+      if (window.matchMedia("screen and (max-width: 994px)").matches) {
+        //framework materialz.jsのcssのcolを付与して対応する
+        let getCol = document.getElementById("ul")[0];
+        // getCol.style.display = 'block';
+
+      }
+    });
+  });
