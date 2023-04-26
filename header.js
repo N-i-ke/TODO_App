605bac84c19bcc7ab2e3d7a857e7b194be0fdb13
N-i-ke <82163707+N-i-ke@users.noreply.github.com>
2023-04-26 18:34:08 +0900

    Update_header.js
    ハンバーガーメニューの開始向きを逆に
    headerのレイアウトとレスポンシブ対応

--- header.js
+++ header.js
@@ -1,7 +1,36 @@
-//jQuery 
+//jquery
 $(document).ready(function () {
     $(".sidenav").sidenav({
-        //右からメニューが追加される
-        edge: "right",
+        edge: "left",
     });
+});
+
+//headerのbreakpointの調整（layout）
+window.addEventListener("DOMContentLoaded", function () {
+
+
+        const getMenuRight = document.getElementsByClassName(
+          "hide-on-med-and-down"
+        )[0];
+        //一時的なlayoutのcssの修正
+        getMenuRight.style.cssText = "height: 64px; align-items: center;";
+
+    
+    window.addEventListener("resize", function () {
+        //headerのresponsive対応
+        if (window.matchMedia("screen and (max-width:994px)").matches) {
+            //frameworkのここが原因
+            let getHamMin = this.document.getElementsByClassName("sidenav-trigger")[0];
+            getHamMin.style.display = 'block';
+        }
+        //994px以上だった時のheaderの対応
+        if (window.matchMedia("screen and (min-width:994px)").matches) {
+            //frameworkのここが原因
+            let getHamMax = this.document.getElementsByClassName("sidenav-trigger")[0];
+            console.log()
+            getHamMax.style.display = 'block';
+        }
+        
+
+  });
 });
