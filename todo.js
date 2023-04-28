4b529a500d1f2eee5b3b547e1f57bade95306919
N-i-ke <82163707+N-i-ke@users.noreply.github.com>
2023-04-28 21:05:43 +0900

    Update_localStarge

--- todo.js
+++ todo.js
@@ -1,45 +1,59 @@
 //Todo.js 
 
-//func clicBtn()
-function clickBtn() {
-  const Input = document.getElementById("new-todo");
-  const Text = Input.value;
-  Input.value = "";
+const addBtn = document.querySelector('#addBtn');
 
-  const createBtn = document.createElement("button");
-  // material.js framework col s2
-  createBtn.className = "add_button col s2";
-  createBtn.innerText = "完了";
+addBtn.addEventListener("click", function () {
+  
+  const Input = document.querySelector("#todoBtn");
+   const Text = Input.value;
+   Input.value = "";
 
-  const createLi = document.createElement("li");
-  // material.js framework col s2
-  createLi.className = "input-field col s2";
-  createLi.innerText = Text;
+   const createBtn = document.createElement("button");
+   createBtn.className = "add_button col s2";
+   createBtn.innerText = "完了";
 
-  //clickされた時にline-throughを持ってなかったら付け加えて持ってたらつける
-  createBtn.onclick = function () {
-    if (createLi.style.textDecoration === "line-through") {
-      createLi.style.textDecoration = "none";
-    } else {
-      createLi.style.textDecoration = "line-through";
-    }
-  };
+   const createLi = document.createElement("li");
+   createLi.className = "input-field col s2";
+   createLi.innerText = Text;
+
+   createBtn.onclick = function () {
+     if (createLi.style.textDecoration === "line-through") {
+       createLi.style.textDecoration = "none";
+     } else {
+       createLi.style.textDecoration = "line-through";
+     }
+   };
 
   document.getElementById("ul").appendChild(createLi);
   document.getElementById("ul").appendChild(createBtn);
-}
-
-
-
-  //追加されるTODOListのresponsive対応
-  window.addEventListener("DOMContentLoaded", function () {
-    window.addEventListener("resize", function () {
   
-      if (window.matchMedia("screen and (max-width: 994px)").matches) {
-        //framework materialz.jsのcssのcolを付与して対応する
-        let getCol = document.getElementById("ul")[0];
-        // getCol.style.display = 'block';
+  //localStrage　04/27
+  const inputData = Text;
+  //inputDataにTodolistの内容を入れる
+  localStorage.setItem('key', inputData);
+  //localStrageのkeyにtodolistのkeyを入れる
+  const saveData = localStorage.getItem('key');
+  //localStrageのkeyを取得
+  Text = saveData;
+});
 
-      }
-    });
+//responsive対応をしていく 04/26
+window.addEventListener("DOMContentLoaded", function () {
+
+  window.addEventListener("resize", function () {
+  
+    if (window.matchMedia("screen and (max-width: 994px)").matches) {
+      //framework materialz.jsのcssのcolを付与して対応する
+      let getCol = document.getElementById("ul")[0];
+      // getCol.style.display = 'block';
+
+    }
   });
+});
+
+window.addEventListener("beforeunload", function () {
+  localStorage.clear();
+});
+
+
+
