39da009409635669048a18b4653d7a5e5d474916
N-i-ke <82163707+N-i-ke@users.noreply.github.com>
2023-04-30 16:27:01 +0900

    Add files via upload

--- todo.js
+++ todo.js
@@ -1,45 +1,87 @@
-//Todo.js 
+//Todo.js
 
-//func clicBtn()
-function clickBtn() {
-  const Input = document.getElementById("new-todo");
-  const Text = Input.value;
-  Input.value = "";
+let main = () => {
+  setupHeader();
+  setupFooter();
+  setupTodoList();
+};
 
-  const createBtn = document.createElement("button");
-  // material.js framework col s2
-  createBtn.className = "add_button col s2";
-  createBtn.innerText = "完了";
-
-  const createLi = document.createElement("li");
-  // material.js framework col s2
-  createLi.className = "input-field col s2";
-  createLi.innerText = Text;
-
-  //clickされた時にline-throughを持ってなかったら付け加えて持ってたらつける
-  createBtn.onclick = function () {
-    if (createLi.style.textDecoration === "line-through") {
-      createLi.style.textDecoration = "none";
-    } else {
-      createLi.style.textDecoration = "line-through";
-    }
-  };
-
-  document.getElementById("ul").appendChild(createLi);
-  document.getElementById("ul").appendChild(createBtn);
-}
-
-
-
-  //追加されるTODOListのresponsive対応
-  window.addEventListener("DOMContentLoaded", function () {
-    window.addEventListener("resize", function () {
-  
-      if (window.matchMedia("screen and (max-width: 994px)").matches) {
-        //framework materialz.jsのcssのcolを付与して対応する
-        let getCol = document.getElementById("ul")[0];
-        // getCol.style.display = 'block';
-
-      }
+let setupHeader = () => {
+  //jquery
+  $(document).ready(function () {
+    $(".sidenav").sidenav({
+      edge: "left",
     });
   });
+};
+
+let setupFooter = () => {
+  //footer
+  const plusBtn = document.querySelector("#plusBtn");
+  const trashBtn = document.querySelector("#trashBtn");
+
+  //footer左ボタンのゴミ箱機能
+  trashBtn.addEventListener("click", function () {
+    const getUlID = document.getElementById("ul");
+    const fisTodoList = getUlID.children[0];
+    const secTodoList = getUlID.children[1];
+
+    //todo-listにcss line-throughが存在したら消えるように実装
+    if (fisTodoList.style.textDecoration === "line-through") {
+      deleteFirstElement = getUlID.removeChild(fisTodoList);
+      deleteSecondElement = getUlID.removeChild(secTodoList);
+    } else {
+      alert("本当に削除してもよろしいですか？");
+    }
+  });
+  //footer右ボタンの要素追加機能
+  plusBtn.addEventListener("click", function () {
+    console.log("plus");
+  });
+};
+
+let setupTodoList = () => {
+  const addBtn = document.querySelector("#addBtn");
+
+  addBtn.addEventListener("click", function () {
+    const Input = document.querySelector("#todoBtn");
+    const Text = Input.value;
+    Input.value = "";
+
+    const createBtn = document.createElement("button");
+    createBtn.className = "add_button col s2";
+    createBtn.id = "todoID";
+    createBtn.innerText = "完了";
+
+    const createLi = document.createElement("li");
+    createLi.className = "input-field col s2";
+    createLi.id = "todoID";
+    createLi.innerText = Text;
+
+    createBtn.onclick = function () {
+      if (createLi.style.textDecoration === "line-through") {
+        createLi.style.textDecoration = "none";
+      } else {
+        createLi.style.textDecoration = "line-through";
+      }
+    };
+
+    document.getElementById("ul").appendChild(createLi);
+    document.getElementById("ul").appendChild(createBtn);
+
+    const inputData = Text;
+    localStorage.setItem("key", inputData);
+
+    //ここで保存がされてるのでここを削除する必要がある
+    // const saveData = localStorage.getItem('key');
+    // Text = saveData;
+
+    //loadしたらローカルストレージの履歴は消える
+    window.addEventListener("beforeunload", function () {
+      localStorage.clear();
+    });
+  });
+};
+
+main();
+
